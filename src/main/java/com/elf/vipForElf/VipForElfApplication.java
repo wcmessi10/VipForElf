311a446881ee566e01b7d7345e1991ddc114c05d
wcmessi10 <cjswogmlen@naver.com>
2025-03-01 19:32:25 +0900

    first commit

---
+++ src/main/java/com/elf/vipForElf/VipForElfApplication.java
@@ -0,0 +1,13 @@
+package com.elf.vipForElf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipForElfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipForElfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elf/vipForElf/VipForElfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elf.vipForElf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipForElfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
